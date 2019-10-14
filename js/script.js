bdb669d47b4cb9fc77a78d65847e3ad60e969000
Russell grant Johnson <932400859@bun2-206-14.local>
2019-10-15 12:40:50 +1300

    fixed up a couple of errors that Beula pointed out

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@ var bestEmployee = 0;
 var projectA = [80 , 75 , 93 , 62];
 var projectB = [53 , 81 , 90 , 60];
 var projectC = [69 , 72 , 93 , 60];
+var projectAverage = [];
 
 for(i=0; i<=3; i++) {
 	// This part of the loop adds each of the employees projects and finds the mean
-	var projectAverage = ((projectA[i] += projectB[i] += projectC[i]) / 3).toFixed(2);
+	var projectAverage = ((projectA[i] + projectB[i] + projectC[i]) / 3).toFixed(2);
 
 	document.getElementById('averagePerformance').innerHTML += employees[i] + ': ' + projectAverage + '% <b>average performance</b> <br>';
 	
