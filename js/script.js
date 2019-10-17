6a2ed4f652170c81294d3aee6bb7c2801d3ac10d
Russell Johnson <rusljohn@gmail.com>
2019-10-17 17:20:44 +1300

    returns outstanding employee and created a form to input employees into the array

--- js/script.js
+++ js/script.js
@@ -1,32 +1,54 @@
 console.log('Algorythm Homework');
 
-var employees = ['Peter' , 'Lucy' , 'Alice' , 'Bryan'];
+var eName = [];
 var bestEmployee = 0;
 
-var projectA = [80 , 75 , 93 , 62];
-var projectB = [53 , 81 , 90 , 60];
-var projectC = [69 , 72 , 93 , 60];
+var projectA = [];
+var projectB = [];
+var projectC = [];
 var projectAverage = [];
 
-for(i=0; i<=3; i++) {
-	// This part of the loop adds each of the employees projects and finds the mean
-	var projectAverage = ((projectA[i] + projectB[i] + projectC[i]) / 3).toFixed(2);
+// Reading form data
+document.getElementById('submitE').addEventListener('click', function() {
+	// Records the name inputted by the user
+	var individualName = document.getElementById('eName').value;
+	console.log(individualName);
 
-	document.getElementById('averagePerformance').innerHTML += employees[i] + ': ' + projectAverage + '% <b>average performance</b> <br>';
-	
-	if(bestEmployee < projectAverage) {
-		bestEmployee = projectAverage[i];
-		var listIndex = i;
+	// Records results for project A
+	var projA = parseInt(document.getElementById('projectA').value);
+	// Records results for project B
+	var projB = parseInt(document.getElementById('projectB').value);
+	// Records results for project C
+	var projC = parseInt(document.getElementById('projectC').value);
+
+	// Finds the average performance of the employee
+	var projAverage = ((projA + projB + projC) / 3).toFixed(2);
+
+	// Placing values into the array
+	eName.push(individualName);
+	projectA.push(projA);
+	projectB.push(projB);
+	projectC.push(projC);
+	projectAverage.push(projAverage);
+	console.log(eName, projectA, projectB, projectC, projectAverage);
+
+	document.getElementById('employeeResults').innerHTML += '<b>' + individualName + ':</b> ' + projA + '% for project A, ' + projB + '% for project B, ' + projC + '% for project C.<br>' + '<b>Project Average:</b> ' + projAverage + '% <br>';
+
+	// Clears the form at the end ready for next input
+	document.getElementById('employeeForm').reset();
+});
+
+// Click to find who the best employee is
+document.getElementById('btnBestEmployee').addEventListener('click', function(){
+	// Takes the length of the 'projectAverage' array as the amount of times the function loops
+	for(var i=0; i<=projectAverage.length; i++) {
+		// Condition - finds the biggest figure in the projectaverage array and compares it to the best employee variable
+		if(bestEmployee < projectAverage[i]){
+			// changes the value of the best employee variable to the project avearge figure and includes the name of the employee that got the corresponding score
+			bestEmployee = projectAverage[i] + '% ' + eName[i];
+		}
 	}
-	document.getElementById('outstandingEmployee').innerHTML = bestEmployee[i];
-} 
-
-// for(j=0; j<=3; j++) {
-// 	// if(bestEmployee < projectAverage) {
-// 	// 	bestEmployee = projectAverage[i];
-// 	// 	var listIndex = i;
-// 	// }
-// 	// document.getElementById('outstandingEmployee').innerHTML = bestEmployee[i];
-// }
+	document.getElementById('bestEmployee').innerHTML = bestEmployee;
+});
 
 
